9a36c1ffb7e352112f91c982286542f6cd3afc2c
Jeegan253 <77412379+Jeegan253@users.noreply.github.com>
2021-01-23 09:54:53 -0500

    Delete Assignment-2.R

--- Assignment-2.R
+++
@@ -1,7 +0,0 @@
-assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22) #creating dataset assignment2
-assignment <- c(9, 18, 27, 36, 45) #another dataset assignment
-mymean <- function(x){return(sum(x)/ length(x))} # creating mean function
-mycalc <- function (x,y){return (sum(x)/ length(y))}
-mymean (assignment2)
-mymean (assignment)
-mycalc (assignment2, assignment)
