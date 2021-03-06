ee17253c1da0081b1e4e9f31b4640e7518830dab
Jeegan253 <77412379+Jeegan253@users.noreply.github.com>
2021-03-06 07:11:50 -0500

    Add files via upload

--- Assignment-8.R
+++ Assignment-8.R
@@ -1,4 +1,4 @@
-setwd("/Users/jeegs/Downloads/Intro to R")
+setwd("/Users/jeegs/Downloads/Intro to R") #setting up working directory
 Ass8 <- read.table(file.choose(), header=T, sep=",")   #reading "Assignment 6 Dataset" into Ass8 dataframe. 
 Ass8
 install.packages("pryr")  #installing pryr and plyr packages
@@ -13,8 +13,10 @@ Avggrades
 #write.table(Avggrades, "Average grades", sep=",")
 #gradesavg<- read.table("Average grades", header=T, sep=",")
 #gradesavg
-Semgrades <- subset(Avggrades, grepl("[i]", Avggrades$Name, ignore.case=T))
+Semgrades <- subset(Avggrades, grepl("[i]", Avggrades$Name, ignore.case=T)) #filtering dataset
 Semgrades
-write.table(Semgrades, "Studentgrades.csv", sep=",")
-Studentgrades <- read.csv (file.choose(), header=T)
+write.csv(Semgrades, "stdgrades.csv") #exporting dataset into .csv file
+read.csv (file.choose(), header=T) #reading stdgrades
+write.table(Semgrades, "Studentgrades.csv", sep=",") #exporting dataset using write.table function and sep argument
+Studentgrades <- read.csv (file.choose(), header=T)  #reading exported csv file.csv file
 Studentgrades
