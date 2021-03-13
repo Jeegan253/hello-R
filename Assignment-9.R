99a9ce27ab5917d122955b83abf3ea3829bfd862
Jeegan253 <77412379+Jeegan253@users.noreply.github.com>
2021-03-13 05:56:59 -0500

    Add files via upload

--- Assignment-9.R
+++ Assignment-9.R
@@ -1,13 +1,13 @@
 Dsmoker<- read.csv(file.choose(), header=T)
 Dsmoker
+Dsmoker$smoke<- factor(Dsmoker$smoke)
 #plotting number of CAD deaths
-plot(Dsmoker$y, col="green", type="o", ylab= "Number of CAD Deaths", main="Number of deaths due to CAD")
+plot(Dsmoker$y, col="lightgrey", type="o",  ylab= "Number of CAD Deaths", main="Number of deaths due to CAD")
 layout_matrix <- matrix(1:2, ncol=2)
 layout_matrix
 layout(layout_matrix)
-#using boxplot for 2 categorical variables
-boxplot(Dsmoker$y~Dsmoker$smoke, col="red", xlab= "Smoke", ylab="Number of CAD Deaths", main="CAD Deaths by Smoking Status")
-boxplot(Dsmoker$y~Dsmoker$age, col="blue", xlab = "Age group", ylab= "Number of Deaths attributed to CAD", main="Number of CAD deaths in doctors by age")
+plot(Dsmoker$smoke,Dsmoker$y, col="red", type="o", xlab= "Smoke", ylab= "Number of CAD Deaths", main="Number of CAD deaths by smoking status")
+boxplot(Dsmoker$y~Dsmoker$age, col="blue", xlab= "Age group", ylab= "Number of CAD Deaths", main="Number of CAD deaths by age group")
 layout_matrix1 <- matrix(1:1, ncol=1)
 layout_matrix
 layout(layout_matrix1)
@@ -20,7 +20,7 @@ plots1 <- xyplot(y~ns, data=Dsmoker,
 plots2 <- xyplot(y~age, data=Dsmoker,
                  grid=FALSE,
                  type=c("p", "r"), col.line="brown", lwd=3)
-plots3 <- xyplot(y~ns, data=Dsmoker,
+plots3 <- xyplot(y~smoke, data=Dsmoker,
                  grid=FALSE,
                  type=c("p", "r"), col.line="brown", lwd=3)
 print(plots1)
@@ -28,17 +28,26 @@ print(plots2)
 print(plots3)
 install.packages("ggplot2")
 library(ggplot2)
-linearR <- lm(y ~ age+smoke, data = Dsmoker)
+linearR <- lm(y ~ smoke + age, data = Dsmoker)
 Dsmoker$predicted<- predict(linearR)
 Dsmoker$residuals <- residuals(linearR)
-ggplot(Dsmoker,aes(x=age+smoke,y=y))+ geom_smooth(method="lm", se=FALSE, color="brown")+ #making a linear regression line
-  geom_segment(aes(xend=age + smoke, yend=predicted), alpha=.2) +
+ggplot(Dsmoker,aes(x= age,y=y))+ geom_smooth(method="lm", se=FALSE, color="brown")+ #making a linear regression line
+  geom_segment(aes(xend= age, yend=predicted), alpha=.2) +
+  geom_point (aes(color=abs(residuals), size=abs(residuals))) + 
+  scale_color_continuous(low="blue", high="red")+   #points are color based on the size of residuals
+  guides(color=FALSE, size=FALSE) +               
+  geom_point(aes(y=predicted), shape=1)+
+  theme_bw()
+ggplot(Dsmoker,aes(x= smoke,y=y))+ geom_smooth(method="lm", se=FALSE, color="brown")+ #making a linear regression line
+  geom_segment(aes(xend= smoke, yend=predicted), alpha=.2) +
  geom_point (aes(color=abs(residuals), size=abs(residuals))) + 
   scale_color_continuous(low="blue", high="red")+   #points are color based on the size of residuals
   guides(color=FALSE, size=FALSE) +               
   geom_point(aes(y=predicted), shape=1)+
   theme_bw()
 plot(linearR, which = 1,col=c("brown"))   #residuals vs fitted line for determining linear relationship
+plot(linearR, which = 2,col=c("brown"))   #residuals vs fitted line for determining linear relationship
+summary(linearR)
 
 
 
