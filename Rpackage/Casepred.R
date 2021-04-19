1d6161ecc241cb844f2348259d2895363c8ccc25
Jeegan253 <jeeganparikh@usf.edu>
2021-04-19 10:01:43 -0400

    Package

---
+++ Rpackage/Casepred.R
@@ -0,0 +1,4 @@
+prediction <- function(x,y) {
+  cases <- 277289 - (5988.21*x) + (3710.91*y)
+  return (cases)
+}
---
+++ caseprediction.Rcheck/00_pkg_src/caseprediction/R/hello.R
@@ -0,0 +1,21 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+prediction <- function(x,y) {
+  cases <- 277289 - (5988.21*x) + (3710.91*y)
+  return (cases)
+}
+prediction(95,84.2)
